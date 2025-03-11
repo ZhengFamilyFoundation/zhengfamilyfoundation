536878214e6908ad4329ee743cfcb9470142ffbc
thealexzheng <alex+github@zheng-dynasty.com>
2025-03-11 11:33:05 -0500

    began modifying index, created about page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About the Zheng Family Foundation</h1>
+      <p>The Zheng Family Foundation is a registered 501(c)(3) and was founded on December 24, 2023 by Alex, Alice, and Justin Zheng</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About the Zheng Family Foundation</title>
+
+export default AboutPage
